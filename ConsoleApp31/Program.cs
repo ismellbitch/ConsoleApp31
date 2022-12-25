71bd5d24366fa6086f68fab3374f78644895fe9e
Артём Коваленко <ak7527203@gmail.com>
2022-12-25 16:28:20 +0200

    Add project files.

---
+++ ConsoleApp31/Program.cs
@@ -0,0 +1,19 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Write("Введите дату в формате dd/mm/yyyy: ");
+        DateTime date = DateTime.Parse(Console.ReadLine());
+        Console.WriteLine(date.ToString("d dddd MMMM yyyy"));
+
+        Console.Write("Введите время старта: ");
+        TimeSpan Start = TimeSpan.Parse(Console.ReadLine());
+        Console.Write("Введите время старта: ");
+        TimeSpan Finish = TimeSpan.Parse(Console.ReadLine());
+
+        double Distance = 3000;
+        double RunningTime = Finish.TotalSeconds - Start.TotalSeconds;
+        double AverageSpeed = Distance / RunningTime;
+        Console.WriteLine($"Время забега: {RunningTime} секунд, Средняя скорость: {AverageSpeed} м/с");
+    }
+}
